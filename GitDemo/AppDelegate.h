85c1d627480313a823b91aa4611dd96d976d66a0
Дмитрий Жердев <dmitryzherdev@MacBook-Air-Dmitrij.local>
2014-11-25 11:29:50 +0300

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by Дмитрий Жердев on 25.11.14.
+//  Copyright (c) 2014 Дмитрий Жердев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by Дмитрий Жердев on 25.11.14.
+//  Copyright (c) 2014 Дмитрий Жердев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
